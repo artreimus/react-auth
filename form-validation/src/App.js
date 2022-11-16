d81077458a2aa1f11ee9e67ba168982336601159
ArthurReimus <arthurreimus1623@gmail.com>
2022-11-16 17:49:16 +0800

    Accomplish form validation

---
+++ form-validation/src/App.js
@@ -0,0 +1,13 @@
+import Login from './Login';
+import Register from './Register';
+
+function App() {
+  return (
+    <main className="App">
+      {/* <Register /> */}
+      <Login />
+    </main>
+  );
+}
+
+export default App;
